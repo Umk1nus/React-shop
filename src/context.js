97df9ca31a4ce234d239e935e616afc348db31a2
Umk1nus <umk1namw@gmail.com>
2022-07-08 00:05:01 +0300

    finish reducer and context

--- src/context.js
+++ src/context.js
@@ -14,8 +14,20 @@ export const ContextProvider = ({children}) => {
 
   const [value, dispatch] = useReducer(reducer, initialState)
 
+  value.addQuantity = (item) => {
+    dispatch({type: 'ADD_QUANTITY', payload: {id: item}})
+  }
+
+  value.removeQuantity = (item) => {
+    dispatch({type: 'REMOVE_QUANTITY', payload: {id: item}})
+  }
+
   value.removeFromBasket = (item) => {
     dispatch({type: 'REMOVE_FROM_BASKET', payload: {id: item}})
+  }
+
+  value.addToBasket = (item) => {
+    dispatch({type: 'ADD_TO_BASKET', payload: {id: item}})
   }
 
   return <ShopContext.Provider value={value}>
